1e736e6443add87121ea914b6f50352a8dfa31f3
akaunderr <akaunderr@gmail.com>
2024-02-27 15:58:51 +0200

    upload aux. files

---
+++ ocmodw.ini.php
@@ -0,0 +1,25 @@
+<?php
+
+define('MODVERS', '1.3.2');
+define('MODYEAR', '2021');
+
+define('AUTHOR', 'Andrii Burkatskyi');
+define('EMAIL', 'ocmod.space@gmail.com');
+
+define('MODLINK', 'https://www.ocmod.space/contact-button');
+define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=43102');
+define('GITLINK', 'https://github.com/ocmod-space/ocmod-contact-button');
+define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension&amp;filter_member=ocmod.space');
+
+define('LICLINK', 'https://raw.githubusercontent.com/ocmod-space/ocmod-contact-button/main/EULA.txt');
+
+///
+
+define('FCLDIR', '_fcl');
+define('FCLIGNORE', '.fclignore'); // ignorelist for fcl
+define('SRCDIR', 'src');
+define('ZIPDIR', 'zip');
+define('ZIPEXT', '.ocmod.zip');
+
+define('MDIR', 'module'); // dir with module
+define('ADIR', 'addons'); // dir with addons
